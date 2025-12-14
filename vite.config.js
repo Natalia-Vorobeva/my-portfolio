639ef7b686c8bdde9a518b454eaf43e42faea9d2
Natalia <vorobjeva.natalia76@gmail.com>
2025-12-14 18:38:44 +0500

    styles

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-	build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Выделяем фоны в отдельный чанк
-          backgrounds: ['./src/components/backgrounds'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Увеличиваем лимит для больших фонов
-  },
+	
 })
