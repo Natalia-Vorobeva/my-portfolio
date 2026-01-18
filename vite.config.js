c952453e5742347f7297b3f6a32186b89f888737
Natalia <vorobjeva.natalia76@gmail.com>
2026-01-19 00:17:57 +0500

    установка зависимости

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     minify: 'terser',
     terserOptions: {
       compress: {
-        drop_console: true, // Удаляем console.log в продакшене
+        drop_console: true,
         drop_debugger: true,
       }
     }
